1a04efcf3a5a58b36f1499ff2508d45b90df622c
efsi2 <129748068+efsi2@users.noreply.github.com>
2023-08-08 08:26:42 -0300

    TJR

--- wordle/public/js/DOM.js
+++ wordle/public/js/DOM.js
@@ -51,66 +51,6 @@ function intialize(){
 }
 
 // Listen for Key Press
-document.addEventListener("keyup", (e) => {
-    if (gameOver) return;
-
-    //alert(e.code)
-    if ("KeyA" <= e.code && e.code <="KeyZ") {
-        if (col < width) {
-            let currTile = document-getElementById(row. toString() + ' - ' + col.toString());
-            if (currTile. innerText =="") {
-                currTile. innerText = e.code[3];
-                col += 1;
-            }
-        }
-    }
-
-    else if (e.code =="Backspace") {
-        if (0 < col && col <= width) {
-            col -=1;
-        }
-        let currTile = document.getElementById(row.toString() + '-' + col.toString());
-        currTile. innerText = "";
-        
-    }
-    else if (e.code == "Enter") {
-        update();
-        row += 1; //start new row
-        col = 0; //start at 0 for new row
-    }
-
-
-    if (!gameOver && row == height) {
-        gameOver = true;
-        document.getElementById("answer").innerText = word;
-    }
-
-})
-
-function update() {
-    let correct = 0;
-    for(let c = 0; c<width; c++){
-        let currTile = document.getElementById(row.toString() + `-` + c.toString());
-        let letter = currTile.innerText;
-        
-        //is it in the correct position?
-        if (word[c] == letter) {
-            tile.classList.add("correct");
-            correct += 1;
-            //is it in the word?
-        }
-        else if (word.includes(letter)) {
-            tile.classList.add("present");
-            //not in the word
-        }
-        else {
-            tile.classList.add("absent");
-        }
-        if (correct == width) {
-            gameOver = true;
-        }
-    }
-}
 
 
 async function putJSON(data) {
@@ -118,7 +58,7 @@ async function putJSON(data) {
 
     try {
       const response = await fetch("/login", {
-        method: "POST", // or 'PUT'
+        method: "PUT", // or 'PUT'
         headers: {
           "Content-Type": "application/json",
         },
