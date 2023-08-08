e156d05e15c29125ef0fc05e30f89051856de71b
FornesAgustina <101611230+FornesAgustina@users.noreply.github.com>
2023-08-08 08:51:48 -0300

    update

--- wordle/public/js/DOM.js
+++ wordle/public/js/DOM.js
@@ -24,32 +24,6 @@ function changeScreen() {
 }
 
 
-var row = 0; //current guest (attempt #)
-var col = 0; //current letter for that attempt
-
-var gameOver = false;
-var word = "SQUID";
-
-window.onload = function(){
-    intialize();
-}
-
-function intialize(){
-    //crate the game board
-    for (let r = 0; r < height; r++){
-        for(let c = 0; c<width; c++){
-            //<span id="0-0" class="tile">P</span>
-            let tile = document.createElement("span");
-            tile.id = r.toString() + "-" + c.toString();
-            tile.classList.add("tile");
-            tile.innerText = "";
-            document.getElementById("tablero").appendChild(tile);
-        }
-    }
-}
-
-// Listen for Key Press
-
 
 async function putJSON(data) {
     //putJSON() es solo el nombre de esta funcion que lo pueden cambiar    
