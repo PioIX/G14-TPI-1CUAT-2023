83bdd570130d056a99dcddaf4a0b02058ef5a6f9
Uchechong410 <fchong@pioix.edu.ar>
2023-08-22 22:04:29 -0300

    update

--- wordle/public/js/wordle.js
+++ wordle/public/js/wordle.js
@@ -1,4 +1,6 @@
 let resultElement = document.querySelector('.result')
+let rowid = 1
+let gameBoard = document.querySelector('.game-board')
 
 let word = 'hola';
 let wordArray = word.toUpperCase().split('');
@@ -6,48 +8,70 @@ console.log(wordArray)
 
 let actualRow = document.querySelector('.row');
 
-/*
-Preguntar mañana porque no funciona el innerhtml
-wordArray.forEach(() => {
-    actualRow.innerHTML += `<input type="text" maxlength="1" class="square"></input>`
-})
-*/
+drawSquares(actualRow);
+input(actualRow)
 
-let squares = document.querySelectorAll('.square')
-squares = [...squares]
 
-let userInput = []
+addFocus(actualRow)
 
-squares.forEach(element =>{
-    element.addEventListener('input', event=>{
-    //recoger lo que el usuario ingresa para guardarlo en el vector "userinput"    
-    userInput.push(event.target.value.toUpperCase())
-    console.log(userInput)
-    if (event.target.nextElementSibling){
-        event.target.nextElementSibling.focus();
-    }else{
-        //Comparar elementos para ver si coinciden (verde)
-        let elementosCorrectos= compareArrays(wordArray, userInput)
-        elementosCorrectos.forEach(element => {
-            squares[element].classList.add('green')
-        })
-        //Cuando todas las letras sean iguales
-        if (elementosCorrectos.length == wordArray.length){
-            resultElement.innerHTML = `<p>Ganaste</p>
-            <div class="mb-3 form-group">
-              <input type="button" class="btn btn-primary" onclick="" value="Siguiente Nivel">
-            </div>`
-        }
-        //Comparar elementos para ver si la letra existe pero no esta en la posicion correcta (amarillo)
-        let ExistePalabras = Existwords(wordArray, userInput)
-        console.log(ExistePalabras)
-        ExistePalabras.forEach(element =>{
-            squares[element].classList.add('gold')
-        })
-        //Crear una linea 
-    }
-    });
-})
+function input(actualRow){
+    let squares = actualRow.querySelectorAll('.square')
+    squares = [...squares]
+
+    let userInput = []
+
+    squares.forEach(element =>{
+        element.addEventListener('input', event=>{
+            //Si no se borro 
+            if (event.inputType !== 'deleteContentBackward'){
+                //recoger lo que el usuario ingresa para guardarlo en el vector "userinput"    
+                userInput.push(event.target.value.toUpperCase())
+                console.log(userInput)
+                if (event.target.nextElementSibling){
+                    event.target.nextElementSibling.focus();
+                }else{
+                    // Crear el array con las letras llenas
+                    let squaresLlenos = actualRow.querySelectorAll('.square')
+                    squaresLlenos = [...squaresField]
+                    let finalUserInput = []
+                    squaresLlenos.forEach(element =>{
+                       finalUserInput.push(element.value.toUpperCase()) 
+                    });
+                    console.log(finalUserInput)
+                    //Comparar elementos para ver si la letra existe pero no esta en la posicion correcta (amarillo)
+                    let ExistePalabras = Existwords(wordArray, finalUserInput)
+                    console.log(ExistePalabras)
+                    ExistePalabras.forEach(element =>{
+                        squares[element].classList.add('gold')
+                    })
+                    //Comparar elementos para ver si coinciden (verde)
+                    let elementosCorrectos= compareArrays(wordArray, finalUserInput)
+                    elementosCorrectos.forEach(element => {
+                        squares[element].classList.add('green')
+                    })
+                    //Cuando todas las letras sean iguales
+                    if (elementosCorrectos.length == wordArray.length){
+                        showResult('Ganaste!')
+                        return;
+                    }
+                    //Crear una fila
+                    let actualRow = createRow()
+
+                    if(!actualRow){
+                        return;
+                    }
+
+                    drawSquares(actualRow)
+                    input(actualRow)
+                    addFocus(actualRow)
+                }
+            }else{
+                userInput.pop();
+            }
+            console.log(userInput)
+        });
+    })
+}
 
 function compareArrays(array1, array2){
     let indexIguales = []
@@ -71,3 +95,46 @@ function Existwords(array1, array2){
     });
     return LetrasEstan;
 }
+
+function createRow(){
+    rowid++
+    if (rowid <= 6){
+        let newRow = document.createElement('div')
+        newRow.classList.add('row')
+        newRow.setAttribute('id', rowid)
+        gameBoard.appendChild(newRow)
+        return newRow
+    }else{
+        showResult(`Intentalo de nuevo, la respuesta correcta era "${word.toUpperCase()}"`)
+    }
+    
+}
+
+function drawSquares(actualRow){
+    wordArray.forEach((item, index) => {
+        if (index === 0){
+            actualRow.innerHTML += `<input type="text" maxlength="1" class="square focus"></input>`
+        } else {
+            actualRow.innerHTML += `<input type="text" maxlength="1" class="square"></input>`
+        }
+    });
+}
+
+function addFocus(actualRow){
+    let focusElement = actualRow.querySelector('.focus')
+    console.log(focusElement)
+    focusElement.focus();
+}
+
+function showResult(text){
+    resultElement.innerHTML = `
+                <p>${text}</p>
+                <div class="mb-3 form-group">
+                <input type="button" class="btn btn-primary" onclick="" value="Siguiente Nivel">
+                </div>`
+    let resetBtn = document.querySelector('.button')
+    resetBtn.addEventListener('click', ()=>{
+        location.reload()
+    })
+}
+// Porque fallan los innerhtml
